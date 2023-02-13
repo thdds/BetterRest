5df3ed8b2e45a4f695016a140301d6703478d3aa
Thaddäus Schima <t.schima@mailbox.org>
2023-02-14 00:03:59 +0100

    added some buttons and suff

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,14 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    var components = Calendar.current.dateComponents([.hour, .minute], from: someDate)
+    let hour = components.hour ?? 0
+    let minute = components.minute ?? 0
+    let date = Calendar.current.date(from: components) ?? Date.now
+    @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Stepper("\(sleepAmount.formatted()) hours", value:$sleepAmount, in: 4...12, step: 0.25)
+            DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...).labelsHidden()
+            Text(Date.now, format: .dateTime.day().month().year())
+            Text(Date.now.formatted(date: .long, time: .shortened))
         }
-        .padding()
     }
 }
 

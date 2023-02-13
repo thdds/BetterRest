f058906da207db186e64a0ef493f690bdc6e6300
Thaddäus Schima <t.schima@mailbox.org>
2023-02-13 21:15:33 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Thaddäus Schima on 13.02.23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
